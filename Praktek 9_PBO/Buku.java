9478e3bdfabafacad92f92ba09a9c8e9fc0d689f
Mohamad Sigit <mohamadsigitriyanto@gmail.com>
2023-07-05 02:55:36 +0700

    test2

--- Praktek 9_PBO/Buku.java
+++ Praktek 9_PBO/Buku.java
@@ -1,15 +1,18 @@
 public class Buku {
     private String idBuku;
     private String namaBuku;
-    
-    public Buku(String idBuku, String namaBuku){
-        this.idBuku=idBuku;
-        this.namaBuku=namaBuku;
+
+    public Buku(String idBuku, String namaBuku) {
+        this.idBuku = idBuku;
+        this.namaBuku = namaBuku;
     }
-    protected String getIdBuku(){
+
+    protected String getIdBuku() {
         return idBuku;
     }
-    protected String getNamaBuku(){
+
+    // test
+    protected String getNamaBuku() {
         return namaBuku;
     }
 }
